2e448eb760dd8d535f059c72d5f2cf6961f4a0a0
STLkru <krunal2shah@gmail.com>
2014-04-26 10:58:44 +0900

    Added hunspell installation check

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,8 +1,7 @@
 # Kru Shah
 # Spell checks words in text file, prompts user to fix or ignore misspelled words, and updates changes as indicated
-# In order to run the script, "hunpell" needs to be installed (sudo apt-get install hunspell)
 
-# To check: apt-cache policy hunspell
+
 
 replaceWord()
 {
@@ -10,7 +9,7 @@ replaceWord()
   echo "Misspelled word $word"  
   read -p "$word is mispelled. Press "Enter" to ignore, "q" to quit, or type replacement here: " correction
   if [[ "$correction" = "q" ]] ; then
-    echo "Exiting without applying any fixes."; 
+    echo "Exiting..."; 
     exit 0
   elif [[ "$correction" = "" ]] ; then
     echo "Ignored misspelled word $word."
@@ -19,19 +18,27 @@ replaceWord()
 }
 
 
+
+if ! which hunspell >/dev/null; then # if hunspell is not already installed, install it
+    echo "First, we need to install hunspell..."
+    sudo apt-get install hunspell # prompt user to install hunspell
+fi
+
+
+echo ""
 echo -n "Enter name of file that you want spell checked: "
 read filename
-
 if [ ! -r $filename ] # If file is not readable, exit
 then 
   echo "ERROR: Cannot read file $filename"; exit 1
 fi
 
 
+
 array=($(hunspell -l < $filename | sed 's/:.*//')) #create an array of misspelled words, using the output from "hunspell -l"
 if [ ${#array[@]} == 0 ] #if the length of the misspelled array is 0, exit
 then 
-  echo "$filename has no errors!"
+  echo "$filename has no spelling errors!"
   exit 0
 else #if array is not empty
   echo ""
