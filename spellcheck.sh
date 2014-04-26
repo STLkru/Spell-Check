cc5f561ae377d403eb90b019a0077b6de6c39e4a
STLkru <krunal2shah@gmail.com>
2014-04-26 09:38:28 +0900

    Fixed file output issues

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,40 +1,34 @@
 # Kru Shah
-# Spell checks words in text file and prompts user to retype misspelled words, or ignore the mispelled words altogether. 
+# Spell checks words in text file, prompts user to fix or ignore misspelled words, and updates changes as indicated
+# In order to run the script, "hunpell" needs to be installed (sudo apt-get install hunspell)
 
-# In order to run the script, "ispell" needs to be installed
-# install ispell: sudo apt-get install ispell/hunspell
+# To check: apt-cache policy hunspell
 
-changerequests="/tmp/$0.$$.sed"
-
-checkSpelling()
+replaceWord()
 {
+  echo ""
   echo "Misspelled word $word"  
   read -p "$word is mispelled. Press "Enter" to ignore, "q" to quit, or type replacement here: " correction
-  
   if [[ "$correction" = "q" ]] ; then
     echo "Exiting without applying any fixes."; 
     exit 0
   elif [[ "$correction" = "" ]] ; then
     echo "Ignored misspelled word $word."
-  else echo "s/$word/$correction/g" >> $changerequests
-  fi 
+  else sed -i -e "s/$word/$correction/g" $filename #fix word in file
+  fi   
 }
 
 
 echo -n "Enter name of file that you want spell checked: "
 read filename
 
-# If file is not readable, return error
-if [ ! -r $filename ] 
+if [ ! -r $filename ] # If file is not readable, exit
 then 
   echo "ERROR: Cannot read file $filename"; exit 1
 fi
 
-touch $changerequests #create new temp file?
 
-
-
-array=($(hunspell -l < $filename | sed 's/:.*//')) #create an array of misspelled words
+array=($(hunspell -l < $filename | sed 's/:.*//')) #create an array of misspelled words, using the output from "hunspell -l"
 if [ ${#array[@]} == 0 ] #if the length of the misspelled array is 0, exit
 then 
   echo "$filename has no errors!"
@@ -43,17 +37,8 @@ else #if array is not empty
   echo ""
   for word in ${array[@]}; #go through all misspelled words in array
   do
-    checkSpelling $word $filename
+    replaceWord $word $filename
   done
-fi
-
-
-
-if [ $(wc -l < $changerequests) -gt 0 ] ; then
-  sed -f $changerequests $filename > $filename.new
-  mv $filename $filename.shp
-  mv $filename.new $filename
-  echo Done. Made $(wc -l < $changerequests) changes.
 fi
 
 exit 0
